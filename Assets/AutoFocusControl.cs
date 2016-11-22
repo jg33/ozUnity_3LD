0108352b3bd7e6a3b88d231a7559fc383d70e428
Kevan Loney <kdloney@gmail.com>
2016-11-22 17:39:25 -0500

    Added Autofocus to ANDROID vuforia

---
+++ Assets/AutoFocusControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class AutoFocusControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		#if UNITY_ANDROID
+		CameraDevice.Instance.SetFocusMode (CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+		#endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
