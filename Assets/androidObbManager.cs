80ba94936fe00cdb0ce6452f61948219e83321c5
Jesse Garrison <jesse@takethefort.com>
2016-11-30 14:05:42 -0500

    obb fix?

--- Assets/androidObbManager.cs
+++ Assets/androidObbManager.cs
@@ -5,15 +5,15 @@ using Vuforia;
 using System.Collections.Generic;
 
 public class androidObbManager : MonoBehaviour {
-	
+
 	void Start () {
 		#if UNITY_ANDROID
-		StartCoroutine( ExtractObbDatasets() );
+		if(PlayerPrefs.GetInt("extractedObb",0)) ExtractObbDatasets() ;
 		#endif
 	}
 	private IEnumerator ExtractObbDatasets () {
-		//Persistent Datapath grabs the file from within the OBB file. 
-		//the path will look something like: 
+		//Persistent Datapath grabs the file from within the OBB file.
+		//the path will look something like:
 		//jar:file:///storage/emulated/0/Android/obb/org.thebuildersassociation.ozBeta2/main.1.org.thebuildersassociation.ozBeta2.obb!/assets/QCAR/ozUnity.xml
 		string toDir = Application.persistentDataPath;
 
@@ -21,11 +21,11 @@ public class androidObbManager : MonoBehaviour {
 		if(!Directory.Exists(Path.GetDirectoryName( toDir + "/QCAR/" ))) Directory.CreateDirectory(Path.GetDirectoryName(toDir + "/QCAR/"));
 		if(!Directory.Exists(Path.GetDirectoryName( toDir + "/Video/"))) Directory.CreateDirectory(Path.GetDirectoryName(toDir + "/Video/"));
 
-		//grab each file and load it. 
+		//grab each file and load it.
 		List<string> filesInOBB = new List<string>();
 		filesInOBB.Add ( Application.streamingAssetsPath + "/QCAR/ozUnity_3LD.xml" );
 		filesInOBB.Add ( Application.streamingAssetsPath + "/QCAR/ozUnity_3LD.dat" );
-		
+
 		filesInOBB.Add ( Application.streamingAssetsPath + "/Video/rainbow_01.mp4" );
 		filesInOBB.Add ( Application.streamingAssetsPath + "/Video/rainbow_02.mp4" );
 		filesInOBB.Add ( Application.streamingAssetsPath + "/Video/rainbow_03.mp4" );
@@ -33,7 +33,7 @@ public class androidObbManager : MonoBehaviour {
 		filesInOBB.Add ( Application.streamingAssetsPath + "/Video/rainbow_05.mp4" );
 		filesInOBB.Add ( Application.streamingAssetsPath + "/Video/rainbow_06.mp4" );
 		filesInOBB.Add ( Application.streamingAssetsPath + "/Video/rainbow_07.mp4" );
-	
+
 		foreach (string filename in filesInOBB) {
 
 			//Debug.LogError("Attempting to load: " + filename + " " + Path.GetFileName(filename));
@@ -56,11 +56,12 @@ public class androidObbManager : MonoBehaviour {
 		}
 		//Tell QCAR to re-search for files; it searches before the files are loaded.
 		GameObject.Find ("ARCamera").GetComponent<DatabaseLoadBehaviour> ().AddOSSpecificExternalDatasetSearchDirs ();
+		PlayerPrefs.SetInt("extractedObb",1);
 	}
 	private void Save(WWW www, string outputPath) {
 		Debug.LogError( "Writing File: " + www.url + " to: " + outputPath);
 		File.WriteAllBytes(outputPath, www.bytes);
-		
+
 		// Verify that the File has been actually stored
 		if(File.Exists(outputPath))
 			Debug.LogWarning("File successfully saved at: " + outputPath);
