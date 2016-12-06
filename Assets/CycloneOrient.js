96c60e836c64c622cbce13b223a037dde5a52ad8
Jesse Garrison <jesse@takethefort.com>
2016-12-06 15:45:58 -0500

    tornado holder, transparent oz flasher, scene 6.

---
+++ Assets/CycloneOrient.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+var cueComponent: cueSystem;
+var headTracker: GameObject;
+var hasOriented: boolean;
+
+
+function Start () {
+	cueComponent = GameObject.Find("Network").GetComponent(cueSystem);
+	headTracker = GameObject.Find("UPFTHeadTracker");
+}
+
+function FixedUpdate () {
+	if(!hasOriented && cueComponent.tornadoState == 4){
+		transform.localPosition = headTracker.transform.parent.parent.localPosition;
+		var targetRotation:Quaternion = Quaternion.Euler(
+			0,
+			headTracker.transform.localRotation.eulerAngles.y,
+			0);
+		transform.localRotation = targetRotation;
+
+		hasOriented = true;
+	}
+}
