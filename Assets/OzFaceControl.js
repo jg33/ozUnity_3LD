62f5532c19c7d863c79bf211ed85f90caceaafb2
Jesse Garrison <jesse@takethefort.com>
2016-12-06 18:11:38 -0500

    oz face work

---
+++ Assets/OzFaceControl.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+var ozFace:GameObject;
+var cueComponent: cueSystem;
+//var animator:Animator;
+
+function Start () {
+	ozFace = GameObject.Find("OzFace");
+	cueComponent = GameObject.Find("Network").GetComponent(cueSystem);
+//	animator = this.gameObject.GetComponent(Animator);
+
+	ozFace.active = false;
+}
+
+function Update () {
+	if(cueComponent.cueNumber==6){
+		//animator.SetBool("active", true);
+		ozFace.active = true;
+	} else{
+		disable();
+	}
+
+}
+
+function disable(){
+		//animator.SetBool("active", false);
+		yield WaitForSeconds(3);
+		ozFace.active = false;
+}
