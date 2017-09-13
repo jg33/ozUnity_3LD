d49fd354325bba06c5523aa84e92390380deb3de
Jesse Garrison <jesse@takethefort.com>
2017-09-13 13:45:13 -0400

    android autofocus fix?

--- Assets/AutoFocusControl.cs
+++ Assets/AutoFocusControl.cs
@@ -9,12 +9,19 @@ public class AutoFocusControl : MonoBehaviour {
 		#if UNITY_ANDROID
 		//StartCoroutine(setAutofocus());
 		CameraDevice.Instance.SetFocusMode (CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
-
+		VuforiaBehaviour.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+		VuforiaBehaviour.Instance.RegisterOnPauseCallback(OnVuforiaStarted);
 		#endif
 	}
 	
 	IEnumerator setAutofocus(){
 		yield return new WaitForSeconds (3);
 		CameraDevice.Instance.SetFocusMode (CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+	}
+
+	private void OnVuforiaStarted() {
+		CameraDevice.Instance.SetFocusMode(
+			CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+		Debug.Log ("set auto focus");
 	}
 }
