f0492acc6566e2e07bfdba7f9eab6f6ffd7986ae
Dan Sakamoto <contact@dansakamoto.com>
2017-09-13 17:07:05 -0400

    Fixed autofocus

--- Assets/AutoFocusControl.cs
+++ Assets/AutoFocusControl.cs
@@ -11,34 +11,12 @@ public class AutoFocusControl : MonoBehaviour {
 		#if UNITY_ANDROID
 		//StartCoroutine(setAutofocus());
 		CameraDevice.Instance.SetFocusMode (CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
-		VuforiaARController.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
-		VuforiaARController.Instance.RegisterOnPauseCallback(OnVuforiaStarted);
-
 		#endif
 	}
-
-	void FixedUpdate(){
-		if (!hasSetFocus) {
-			hasSetFocus = CameraDevice.Instance.SetFocusMode (
-				CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
-			Debug.Log ("setting focus in update... returned: "+hasSetFocus);
-		}
-	}
-	
+		
 	IEnumerator setAutofocus(){
 		yield return new WaitForSeconds (3);
 		CameraDevice.Instance.SetFocusMode (CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
 	}
 
-	private void OnVuforiaStarted() {
-		hasSetFocus = CameraDevice.Instance.SetFocusMode(
-			CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
-		Debug.Log ("set auto focus (start callback)");
-	}
-		
-	private void OnVuforiaStarted(bool boo) {
-		hasSetFocus = CameraDevice.Instance.SetFocusMode(
-			CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
-		Debug.Log ("set auto focus (pause callback)");
-	}
 }
