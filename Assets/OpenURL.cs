7d201473804d5bb60dcb5d411265773533aeafb8
Jesse Garrison <jesse@takethefort.com>
2019-11-26 10:45:11 -0500

    [LS] new menu stuff

---
+++ Assets/OpenURL.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class OpenURL : MonoBehaviour 
+{
+	public string Url;
+
+	public void Open()
+	{
+		Application.OpenURL("https://www.elementsofoz.com/privacy-policy");
+	}
+
+}
